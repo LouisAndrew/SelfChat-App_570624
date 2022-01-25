5a464fa2ee6170df49b2620a8622c6509e490f1d
Louis Andrew <louis.andrew@share-now.com>
2022-01-25 11:33:48 +0100

    Initial Commit

---
+++ SelfChat-App_570624/SelfChat_App_570624App.swift
@@ -0,0 +1,17 @@
+//
+//  SelfChat_App_570624App.swift
+//  SelfChat-App_570624
+//
+//  Created by Louis Andrew on 25.01.22.
+//
+
+import SwiftUI
+
+@main
+struct SelfChat_App_570624App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
