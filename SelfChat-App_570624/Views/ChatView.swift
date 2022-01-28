3b5909f78c267287698724924ccbeb7a93957595
Louis Andrew <louisandrew3@gmail.com>
2022-01-28 14:53:10 +0100

    feat(chat-history): finalised chat history controller

--- SelfChat-App_570624/Views/ChatView.swift
+++ SelfChat-App_570624/Views/ChatView.swift
@@ -7,11 +7,65 @@
 
 import SwiftUI
 
-struct ChatView: View {
-    var chatPartner: ChatPartner
+struct MessageView: View {
+    var message: Message
     
     var body: some View {
-        Text("Chatting with: " + chatPartner.name)
+        Text(message.content)
+            .foregroundColor(message.sender ? .black : .white)
+            .background(message.sender ? .gray : .blue)
+    }
+}
+
+struct ChatView: View {
+    
+    var chatPartner: ChatPartner
+    @ObservedObject var chatHistoryController: ChatHistoryController
+    
+    @State var hasError: Bool = false
+    @State var errorMessage: String = ""
+    
+    init(chatPartner: ChatPartner) {
+        chatHistoryController = ChatHistoryController(chatPartner: chatPartner)
+        self.chatPartner = chatPartner
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                if hasError {
+                    Text("Has error")
+                    Text(errorMessage)
+                }
+                
+                ForEach(chatHistoryController.chatHistory.messages, id: \.self) { message in
+                    MessageView(message: message)
+                }
+                
+                Button(action: {
+                    do {
+                        try chatHistoryController.addNewMessage(message: Message(content: "Hi", sender:  true))
+                    } catch {
+                        hasError = true
+                        errorMessage = "Err: \(error)"
+                    }
+                }, label: {
+                    Text("Send something")
+                })
+                
+                Button(action: {
+                    do {
+                        try chatHistoryController.addNewMessage(message: Message(content: "Hello!", sender:  false))
+                    } catch {
+                        hasError = true
+                        errorMessage = "Err: \(error)"
+                    }
+                }, label: {
+                    Text("Receive something")
+                })
+            }
+            .navigationTitle("Chatting with " + chatPartner.name)
+        }
     }
 }
 
